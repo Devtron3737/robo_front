bc43aa2525f8834212aa30b69b75e910855d026e
Devtron718 <devin.miranda@gmail.com>
2022-04-19 17:55:01 -0400

    parsing csv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+
+import './App.css'
+import { parse } from '@vanillaes/csv';
+// import * as fs from 'fs';
+// import fs from 'fs'
+import roboProfCsv from './robo_profs.csv'
+import Papa from 'papaparse';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState([]);
+
+    console.log("hiiii")
+
+    useEffect(() => {
+        // fs.readFile('robo_profs.csv', 'utf8', function (err, data) {
+        console.log("!!!data");
+        console.log(roboProfCsv);
+
+        Papa.parse(roboProfCsv, {
+            download: true,
+            complete: function (input) {
+                const records = input.data;
+                console.log("!!!PARSED data");
+                console.log(records);
+            }
+        });
+
+        // const parsed = parse(roboProfCsv)
+        // console.log("THIS IS PARSED")
+        // console.log(parsed);
+        // });
+    })
+
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
