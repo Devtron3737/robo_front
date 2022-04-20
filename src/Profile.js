8df432a2a6828b7aca64667c1c0fa0c570db4ce7
Devtron718 <devin.miranda@gmail.com>
2022-04-20 11:55:28 -0600

    done

--- src/Profile.js
+++ src/Profile.js
@@ -1,21 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import './App.css'
 
-function Profile({ handleNext, handleBack, profile }) {
+function Profile({ handleNext, profile }) {
     let name, age, details, aboutMe;
     [name, age, details, aboutMe] = profile
 
 
     return (
         <div>
-            <div>{name}</div>
-            <div>{age}</div>
-            <div>{details}</div>
-            <div>{aboutMe}</div>
+            <div className="name-age-container">
+                <div className="name-age">
+                    🤖 {name}, {age}
+                </div>
+            </div>
+            <div className="details-container">
+                <div className="details">
+                    {details}
+                </div>
+            </div>
+            <div className="about-me-text-container">
+                About Me:
+            </div>
+            <div className="about-me-container">
+                <div className="about-me">
+                    {aboutMe}
+                </div>
+            </div>
 
-            <div onClick={handleBack}>PREVIOUS PROFILE</div>
-            <div onClick={handleNext}>NEXT PROFILE</div>
+            <div className="next-button-container">
+                <div className="next-button-subcontainer">
+                    <div className='next-button' onClick={handleNext}>
+                        Proceed...
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
